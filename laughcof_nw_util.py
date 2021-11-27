08581551df26e894d33a1262835d100b09b70efd
charaguru <charaguru@gmail.com>
2021-11-27 20:08:10 +0900

    move

--- laughcof_nw_util.py
+++ laughcof_nw_util.py
@@ -2,42 +2,28 @@ import discord
 from datetime import datetime, timedelta
 import os
 import traceback
+import yaml
 
+settings = {}
 
-# token = os.environ['DISCORD_BOT_TOKEN']
-token = 'OTEzNzcxMTMxNjEyMzAzMzYx.YaDVrg.I2m5KKdqWjYcxXNCQWa9PUAb860' # bot トークン
+with open('./settings.yaml', 'r', encoding="utf-8") as f:
+    settings = yaml.safe_load(f)['settings']
 
-# server_id = os.environ['SERVER_ID']
+token = settings['token']
 
-# server_id = 749892029995024414
+server_id = int(settings['server_id'])
 
-# テスト用
-server_id = 519741240611438592
+attack_tx_id = int(settings['attack_tx_id'])
+defence_tx_id = int(settings['defence_tx_id'])
+flanker_tx_id = int(settings['flanker_tx_id'])
 
-# attack_tx_id = 893449686730559488
-# defence_tx_id = 911447824376414228
-# flanker_tx_id = 911446932667371550
+attack_vc_id = int(settings['attack_vc_id'])
+defence_vc_id = int(settings['defence_vc_id'])
+flanker_vc_id = int(settings['flanker_vc_id'])
 
-# テスト用
-attack_tx_id = 913777189571424316
-defence_tx_id = 913777204851269712
-flanker_tx_id = 913777225315287060
-
-# attack_vc_id = 893449861712736326
-# defence_vc_id = 911235942998765658
-# flanker_vc_id = 911194715444101140
-
-# テスト用
-attack_vc_id = 913777726060642314
-defence_vc_id = 913777742858838066
-flanker_vc_id = 913777759015288862
+listen_only_member_ids = settings['listen_only_member_ids']
 
 default_party = {}
-
-listen_only_member_ids = (
-    911883459465728030, # きゃら
-    911274250533486613  # えんそく
-)
 
 client = discord.Client()
 
@@ -50,9 +36,6 @@ async def on_ready():
 # コマンド設定
 @client.event
 async def on_message(message):
-    print('Message: ')
-    print(message.content)
-
     # メッセージの送信者がbot自身で無い
     if message.author == client.user:
         return
@@ -61,21 +44,28 @@ async def on_message(message):
     if message.channel.id not in (attack_tx_id, defence_tx_id, flanker_tx_id):
         return 
 
-    # テスト
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello World!')
-
     # 全集合
     if message.content.startswith('$group all'):
         attack_vc = client.get_channel(attack_vc_id)
         defence_vc = client.get_channel(defence_vc_id)
         flanker_vc = client.get_channel(flanker_vc_id)
 
+        print('---------')
+        print(attack_vc)
+        print(defence_vc)
+        print(flanker_vc)
+
+        print(attack_vc.members)
+        print(defence_vc.members)
+        print(flanker_vc.members)
+        print('---------')
+
         for member in attack_vc.members:
             default_party[member] = attack_vc
             await member.move_to(attack_vc)
         
         for member in defence_vc.members:
+            print(member)
             default_party[member] = defence_vc
             await member.move_to(attack_vc)
 
